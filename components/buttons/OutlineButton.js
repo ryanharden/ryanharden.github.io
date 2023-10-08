bff1db258ed056f7f104209b3e2a657ca5879d83
Ryan Harden <ryanharden20@gmail.com>
2023-10-07 21:12:31 -0700

    old site back

--- components/buttons/OutlineButton.js
+++
@@ -1,9 +0,0 @@
-import styles from "./outlinebutton.module.scss";
-
-export const OutlineButton = ({ children, onClick }) => {
-  return (
-    <button onClick={onClick} className={styles.outlineButton}>
-      {children}
-    </button>
-  );
-};
--- components/buttons/StandardButton.js
+++
@@ -1,9 +0,0 @@
-import styles from "./standardbutton.module.scss";
-
-export const StandardButton = ({ children, onClick }) => {
-  return (
-    <button onClick={onClick} className={styles.standardButton}>
-      {children}
-    </button>
-  );
-};
